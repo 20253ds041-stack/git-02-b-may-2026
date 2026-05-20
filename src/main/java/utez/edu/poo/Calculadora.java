fca0174879023a66ece4c084a390c1482e10a60a
Neri Valdez <20253ds041@utez.edu.mx>
2026-05-20 13:50:07 -0600

    [NEW] clase calculadora y constructor

---
+++ src/main/java/utez/edu/poo/Calculadora.java
@@ -0,0 +1,14 @@
+package utez.edu.poo;
+
+public class Calculadora {
+
+    private double num1;
+    private double num2;
+
+    public Calculadora(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+
+}
